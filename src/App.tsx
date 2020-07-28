7c75defdb802ad7c815cec3190957e57698b5c1a
Erick Malta <erickmalta100@gmail.com>
2020-07-28 18:42:04 -0300

    feat: :ok_hand: add nav with logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
-import React from "react";
+import React from 'react';
+import { Menu } from './components';
 
 const App: React.FC = () => {
-  return <div>Hello World</div>;
+  return (
+    <>
+      <Menu />
+    </>
+  );
 };
 
 export default App;
---
+++ src/components/Menu/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Logo from '../../assets/img/Logo.png';
+
+const Menu: React.FC = () => {
+  return (
+    <nav>
+      <img src={Logo} alt="CodeFlix" />
+    </nav>
+  );
+};
+
+export default Menu;
