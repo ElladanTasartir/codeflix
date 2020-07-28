ce0e332fcb04b0dd8c34014e3bfce3920000c9ae
Erick Malta <erickmalta100@gmail.com>
2020-07-28 20:16:09 -0300

    feat: :ok_hand: components for the home and data done

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import { Menu } from './components';
+import BannerMain from './components/BannerMain';
+import dadosIniciais from './data/dados_iniciais.json';
 
 const App: React.FC = () => {
   return (
-    <>
+    <div>
       <Menu />
-    </>
+
+      <BannerMain
+        videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
+        url={dadosIniciais.categorias[0].videos[0].url}
+        videoDescription={"O que é Front-end?"}
+        />
+    </div>
   );
 };
 
--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import Logo from '../../assets/img/Logo.png';
 import './Menu.css';
+import Button from '../Button';
 
 const Menu: React.FC = () => {
   return (
-    <nav>
-      <img className="Logo" src={Logo} alt="CodeFlix Logo" />
+    <nav className="Menu">
+      <a href="/">
+        <img className="Logo" src={Logo} alt="CodeFlix Logo" />
+      </a>
+
+      {/* as Adiciona comportamento de uma outra tag à tag que possuímos, no caso um button
+          que se comporta como um a, isso não é exclusivo do styled-components */}
+      <Button as="a" className="ButtonLink" href="/">Novo vídeo</Button>
     </nav>
   );
 };
