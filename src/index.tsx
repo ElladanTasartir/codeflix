773315936e353f9a1b2edefdddc0e75df3fea527
Erick Malta <erickmalta100@gmail.com>
2020-07-29 22:28:53 -0300

    feat: :ok_hand: add router to index

--- src/index.tsx
+++ src/index.tsx
@@ -2,5 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <BrowserRouter>
+    <Switch>
+      <Route exact path="/" component={App} />
+    </Switch>
+  </BrowserRouter>,
+  document.getElementById('root')
+);
