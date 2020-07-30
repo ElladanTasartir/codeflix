9783109f53bbd7637e6c52755d4f1cd9eb3f9815
Erick Malta <erickmalta100@gmail.com>
2020-07-30 18:08:47 -0300

    feat: :ok_hand: add CadastroVideo e 404 to routes

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import App from './App';
+import CadastroVideo from './pages/cadastro/Video';
+import './index.css';
 
 ReactDOM.render(
   <BrowserRouter>
     <Switch>
       <Route exact path="/" component={App} />
+      <Route exact path="/cadastro/video" component={CadastroVideo} />
+      <Route component={() => <div>Página 404</div>} />
     </Switch>
   </BrowserRouter>,
   document.getElementById('root')
