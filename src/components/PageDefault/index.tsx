c6e62188a83f5b581d52092b046563f495ee63c3
Erick Malta <erickmalta100@gmail.com>
2020-08-01 21:36:47 -0300

    feat: :ok_hand: separated default page from router

--- src/components/PageDefault/index.tsx
+++ src/components/PageDefault/index.tsx
@@ -1,22 +1,13 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import Home from '../../pages/Home';
-import CadastroVideo from '../../pages/cadastro/Video';
 import Menu from '../Menu';
 import Footer from '../Footer';
 
-const PageDefault: React.FC = () => {
+const PageDefault: React.FC = ({ children }) => {
   return (
     <>
-      <BrowserRouter>
-        <Menu />
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/cadastro/video" component={CadastroVideo} />
-          <Route component={() => <div>Página 404</div>} />
-        </Switch>
-        <Footer />
-      </BrowserRouter>
+      <Menu />
+      {children}
+      <Footer />
     </>
   );
 };
